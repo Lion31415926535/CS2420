5fc760450e9c47a515f626680dc70c4757d82265
Jacob Lyon <A02371437@aggies.usu.edu>
2025-09-08 13:35:34 -0600

    Recursive Flooding

--- Flooding/src/Flooding.java
+++ Flooding/src/Flooding.java
@@ -31,7 +31,27 @@ public class Flooding {
 
     void markFloodedR(GridLocation g) {
         try {
-        // Write recursive code to markFlooded areas
+            // First, check if current tile is below water
+            // If so, mark as flooded
+            if (terrain[g.row][g.col] <= height) {
+                flooded[g.row][g.col] = true;
+            }
+
+            GridLocation[] neighbors = {
+                    new GridLocation(g.row + 1, g.col),
+                    new GridLocation(g.row - 1, g.col),
+                    new GridLocation(g.row, g.col + 1),
+                    new GridLocation(g.row, g.col - 1)};
+            // Next, for each neighbor, check if a neighbor is valid
+            // Then call markFloodedR for the neighbor
+            for (GridLocation n : neighbors) {
+                if (validNeighbor(n) && !flooded[n.row][n.col]) {
+                    if (terrain[n.row][n.col] <= height) {
+                        markFloodedR(n);
+                    }
+                }
+            }
+
         } catch (StackOverflowError e) {
             System.err.println("Stack Overflow");
             System.exit(0);
