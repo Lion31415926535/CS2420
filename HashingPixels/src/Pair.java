332f9408af70d38355503daf3ec8e7e5af138330
Jacob Lyon <A02371437@aggies.usu.edu>
2025-10-14 16:44:37 -0600

    Fixed Pair class to override .equals

--- HashingPixels/src/Pair.java
+++ HashingPixels/src/Pair.java
@@ -1,14 +1,14 @@
-public class Pair<T1,T2> {
-    private T1 first;
-    private T2 second;
+public class Pair {
+    private String first;
+    private int second;
 
-    public T1 get1(){
+    public String get1(){
         return this.first;
     }
-    public T2 get2(){
+    public int get2(){
         return this.second;
     }
-    Pair( T1 a, T2 b){
+    Pair( String a, int b){
         this.first = a;
         this.second = b;
       }
@@ -21,13 +21,13 @@ public class Pair<T1,T2> {
      * Changes the second variable to a new value
      * @param newVal New value for the second variable
      */
-    public void changeSecond(T2 newVal) {
+    public void changeSecond(int newVal) {
         this.second = newVal;
     }
 
     public static void main(String args[]){
-        Pair<String,Integer> class1 = new Pair<>( "CS", 2420);
-        Pair<String,Integer> class2 = new Pair<>( "Math", 2610);
+        Pair class1 = new Pair( "CS", 2420);
+        Pair class2 = new Pair( "Math", 2610);
         System.out.println("I am taking " + class1 + class2);
     }
 
@@ -38,5 +38,31 @@ public class Pair<T1,T2> {
     @Override
     public int hashCode() {
         return this.first.hashCode();
+    }
+
+    /**
+     * Compares the first value (key) of the pairs
+     * @param otherPair The other Pair to compare to
+     * @return negative if less than, positive if greater than, and 0 if equal
+     */
+    public int compareTo(Pair otherPair) {
+        return this.first.compareTo(otherPair.first);
+    }
+
+    /**
+     * Checks if this pair is equal to another object
+     * If the object isn't a Pair type it returns false
+     * If the object is a Pair type, then it compares the first value
+     * @param otherObject The object to be compared
+     * @return true if they are equal, otherwise false
+     */
+    @Override
+    public boolean equals(Object otherObject) {
+        // Make sure you are comparing to a Pair object
+        if (!(otherObject instanceof Pair)) {
+            return false;
+        }
+        Pair otherPair = (Pair) otherObject;
+        return this.first.compareTo(otherPair.first) == 0;
     }
 }
