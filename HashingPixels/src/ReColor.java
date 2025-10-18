da3e070b00963c8539608052ccfd4a7f720f2d57
Jacob Lyon <A02371437@aggies.usu.edu>
2025-10-18 17:06:05 -0600

    Made a hashtable of ColorMaps and a ColorTable

--- HashingPixels/src/ReColor.java
+++ HashingPixels/src/ReColor.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -62,15 +63,65 @@ public class ReColor {
         }
     }
 
+    public void getNewImage() {
+        // Make a hash table of all color maps
+        // Gets a list of all color maps in the hash table
+        // Makes color table of most prevalent color maps
+        HashTable<ColorMap> map = this.getColorMaps();
+        ArrayList<ColorMap> list = map.getAll();
+        list.sort(Comparator.reverseOrder());
+
+        ColorMap[] colorTable = new ColorMap[this.colorLimit];
+        for (int i = 0; i < colorTable.length; i++) {
+            colorTable[i] = list.get(i);
+        }
+
+
+    }
+
+    /**
+     * Creates a hash table of all the color maps in the image
+     * Keeps track of the count of occurrences of each color map
+     * @return A Hash table with all color maps from image
+     */
+    private HashTable<ColorMap> getColorMaps() {
+        HashTable<ColorMap> colorMaps = new HashTable<>();
+        BufferedImage newImg = new BufferedImage(this.width, this.height, this.img.getType());
+        for (int x = 0; x < this.width; x++) {
+            for (int y = 0; y < this.height; y++) {
+                // Get value for each color
+                int p = img.getRGB(x, y);
+                int alpha = (p >> 24) & 0xff; //transparency measure
+                int r = (p >> 16) & 0xff;  //red
+                int g = (p >> 8) & 0xff;   //green
+                int b = p & 0xff;
+
+                // Finds the color map for the current pixel
+                // If it is in the hash table then increase the count
+                // If not, then insert in the hash table
+                ColorMap currentMap = new ColorMap(alpha, r, g, b, this.cube);
+                ColorMap found = colorMaps.find(currentMap);
+                if (found == null) {
+                    colorMaps.insert(currentMap);
+                } else {
+                    found.occurCt++;
+                }
+            }
+        }
+        return colorMaps;
+    }
+
 
     public static void main(String[] args) {
         String[] files = {"chart.png", "bird.png", "butterfly.png", "cat.png", "dice.png", "flowers.png"};
         int[] colorMax = {5, 100, 100, 25, 6, 40};
 
-        for (int i = 0; i < files.length; i++) {
+        for (int i = 0; i < 1; i++) {
 
             ReColor r = new ReColor(files[i], 6, colorMax[i]);
-            r.makeRed();
+            r.getNewImage();
+
+//            r.makeRed();
             //r.grabColors();
         }
     }
